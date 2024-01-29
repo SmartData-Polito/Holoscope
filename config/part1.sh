dda500f4bba0fa6c6b89f3ed2df435dc311659ce
aleayalag09 <aleayalag09@javerianacali.edu.co>
2024-01-29 16:26:17 +0100

    backup files

---
+++ config/part1.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+# Install WireGuard
+sudo apt-get install -y wireguard
+
+# Generate WireGuard keys
+wg genkey | tee privatekey | wg pubkey > publickey
+
+# Cat public key and display email
+printf "\n\nSend an email to hiabpolito@gmail.com with the your public key: $(cat publickey)\n\n"
---
+++ config/part2.sh
@@ -0,0 +1,38 @@
+# Create WireGuard configuration file
+sudo bash -c "cat > /etc/wireguard/wg0.conf << EOF
+[Interface]
+Address = 10.0.0.6/24
+PrivateKey = $(cat privatekey)
+
+[Peer]
+PublicKey = VVx1dnVeVgT4vw7qVYONY5e+8oyt8aK0CHpdKIto0Q4=
+Endpoint = kubernetes.polito.it:51820
+AllowedIPs = 10.0.0.1/32
+EOF"
+
+# Create WireGuard Up service file
+sudo bash -c "cat > /etc/systemd/system/wireguard-up.service << EOF
+[Unit]
+After=network.target
+
+[Service]
+Type=simple
+ExecStart=/usr/bin/wg-quick up wg0
+
+[Install]
+WantedBy=default.target
+EOF"
+
+# Reload systemd and start WireGuard service
+sudo systemctl enable wireguard-up.service
+sudo systemctl start wireguard-up.service
+
+# Install K3s with WireGuard
+curl -sfL https://get.k3s.io | K3S_URL=https://10.0.0.1:6443 K3S_TOKEN=K1047421038c1a6b28861b8d4ac1e001f0ce65c8201606732dc444c47c837c3864f::server:ca4392a1e9d257daeaf4ab0d57237226 K3S_NODE_NAME=name-node sh - 
+
+# Configure flannel in node
+echo "ExecStart=/usr/local/bin/k3s agent --node-ip 10.0.0.6 --flannel-iface=wg0" | sudo tee -a /etc/systemd/system/k3s-agent.service
+
+# Restart k3s service
+sudo systemctl daemon-reload 
+sudo systemctl restart k3s-agent.service
